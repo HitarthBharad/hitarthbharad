7cd36bcfe90e7977e8ba9b526b92c7151b3a185d
Hitarth Bharad <hbbharad@gmail.com>
2025-07-03 11:14:20 -0700

    build fix

--- components/client-layout.tsx
+++ components/client-layout.tsx
@@ -4,33 +4,33 @@ import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 
 const ClientLayout = () => {
-    const route = usePathname();
+  const route = usePathname();
 
-    useEffect(() => {
-        if (!route) return;
+  useEffect(() => {
+    if (!route) return;
 
-        const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        const locale = navigator.language;
-        const screenWidth = window.screen.width;
-        const screenHeight = window.screen.height;
-        const colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const locale = navigator.language;
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+    const colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
-        // fetch("/api/visitor", {
-        //     method: "POST",
-        //     headers: { "Content-Type": "application/json" , "Authorization": `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`},
-        //     body: JSON.stringify({
-        //         route,
-        //         timezone,
-        //         locale,
-        //         screenWidth,
-        //         screenHeight,
-        //         colorScheme
-        //     })
-        // })
+    fetch("/api/visitor", {
+      method: "POST",
+      headers: { "Content-Type": "application/json", "Authorization": `Bearer ${process.env.NEXT_PUBLIC_API_KEY}` },
+      body: JSON.stringify({
+        route,
+        timezone,
+        locale,
+        screenWidth,
+        screenHeight,
+        colorScheme
+      })
+    })
 
-    }, [route]);
+  }, [route]);
 
-    return null;
+  return null;
 }
 
 export default ClientLayout;
