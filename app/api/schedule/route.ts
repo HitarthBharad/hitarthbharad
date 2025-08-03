3a4eaed1a4431bdd42ca1a88e8f2dcb1f87afa34
Hitarth Bharad <hbbharad@gmail.com>
2025-08-03 16:33:30 -0700

    Build Fix

--- app/api/schedule/route.ts
+++ app/api/schedule/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import wod_process from "@/lib/wod-process";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   await wod_process();
   return NextResponse.json({ msg: 'OK' });
 };
