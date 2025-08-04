01220ab526db2cc756fe6873d20756b59cbba8a9
Hitarth Bharad <hbbharad@gmail.com>
2025-08-04 13:37:48 -0700

    Send Email aegion api

---
+++ app/api/send-email/route.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from 'next/server';
+import nodemailer from "nodemailer";
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+
+async function sendEmail(subject: string, content: string) {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: Number(process.env.EMAIL_PORT),
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  } as SMTPTransport.Options);
+
+
+  await transporter.sendMail({
+    from: process.env.EMAIL_FROM,
+    to: 'hbbharad@gmail.com',
+    subject,
+    text: content
+  });
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const { message, username, subject } = body;
+
+  const content = `Hello ${username}, \n\n` + message + `\n\nBest Regards,\nTeam Aegion.`
+
+  await sendEmail(subject, content);
+
+  return NextResponse.json({ success: true }, { status: 201 });
+}
